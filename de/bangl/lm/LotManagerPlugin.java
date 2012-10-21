88ebf1bd4668e13523983ae80460dfe34c3502cc
BangL <henno.rickowski@googlemail.com>
2012-10-21 15:39:57 +0200

    GPLv3.0

--- de/bangl/lm/LotManagerPlugin.java
+++ de/bangl/lm/LotManagerPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 BangL <henno.rickowski@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.bangl.lm;
 
 import com.sk89q.worldguard.domains.DefaultDomain;
@@ -35,7 +51,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
- * @author BangL
+ * @author BangL <henno.rickowski@googlemail.com>
  */
 public class LotManagerPlugin extends JavaPlugin {
     public PluginManager pm;
